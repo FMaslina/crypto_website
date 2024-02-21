3d3d2903e699daa0c2f60c967d09ec3cdfe693c6
FMaslina <FMaslina@yandex.ru>
2024-02-21 18:02:34 +0700

    Change PostModel fields

--- posts/models.py
+++ posts/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Post(models.Model):
     text = models.TextField(null=True, blank=True, verbose_name='Текст')
     pub_date = models.DateTimeField(auto_now_add=True, verbose_name='Дата публикации')
-    image = models.ImageField(upload_to='images', null=True, blank=True, verbose_name='Изображение')
+    image = models.FileField(upload_to='images', null=True, blank=True, verbose_name='Изображение')
     video = models.FileField(upload_to='videos', null=True, blank=True, verbose_name='Видео')
 
     class Meta:
