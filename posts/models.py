8d206197970f752886754cdad67a553a9c21bec5
beevel2 <rumaysa.azamatova03@gmail.com>
2024-03-04 07:12:14 +0000

    Update post model

--- posts/models.py
+++ posts/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 
 class Post(models.Model):
+    title = models.TextField(null=True, blank=True, verbose_name='Заголовок')
     text = models.TextField(null=True, blank=True, verbose_name='Текст')
     pub_date = models.DateTimeField(auto_now_add=True, verbose_name='Дата публикации')
     image = models.FileField(upload_to='images', null=True, blank=True, verbose_name='Изображение')
