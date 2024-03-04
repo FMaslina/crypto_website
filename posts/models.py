675494707f44b3b7ad86c28036aca25d94099cc1
FMaslina <FMaslina@yandex.ru>
2024-03-04 20:00:25 +0700

    Add text on different languages fields to Post model

--- posts/models.py
+++ posts/models.py
@@ -1,9 +1,13 @@
 from django.db import models
+from .utils import get_translate
 
 
 class Post(models.Model):
     title = models.TextField(null=True, blank=True, verbose_name='Заголовок')
     text = models.TextField(null=True, blank=True, verbose_name='Текст')
+    text_ru = models.TextField(null=True, blank=True, verbose_name='Текст (RU)')
+    text_en = models.TextField(null=True, blank=True, verbose_name='Текст (EN)')
+    text_pl = models.TextField(null=True, blank=True, verbose_name='Текст (PL)')
     pub_date = models.DateTimeField(auto_now_add=True, verbose_name='Дата публикации')
     image = models.FileField(upload_to='images', null=True, blank=True, verbose_name='Изображение')
     video = models.FileField(upload_to='videos', null=True, blank=True, verbose_name='Видео')
@@ -11,3 +15,17 @@ class Post(models.Model):
     class Meta:
         verbose_name = 'Новость'
         verbose_name_plural = 'Новости'
+
+    def save(self, *args, **kwargs):
+        if self.text:
+            ru_text = get_translate('autodetect', 'ru', self.text)
+            en_text = get_translate('autodetect', 'en', self.text)
+            pl_text = get_translate('autodetect', 'pl', self.text)
+
+            equal_languages_error_message = "PLEASE SELECT TWO DISTINCT LANGUAGES"
+
+            self.text_ru = ru_text if ru_text != equal_languages_error_message else self.text
+            self.text_en = en_text if en_text != equal_languages_error_message else self.text
+            self.text_pl = pl_text if pl_text != equal_languages_error_message else self.text
+
+        super(Post, self).save(*args, **kwargs)
