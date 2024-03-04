1c136574e12501bc5a577f4e78d3bb6ca3363735
FMaslina <FMaslina@yandex.ru>
2024-03-04 20:01:04 +0700

    Create custom PostSerializer

--- posts/serializers.py
+++ posts/serializers.py
@@ -5,4 +5,15 @@ from posts.models import Post
 class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
-        fields = '__all__'
+        fields = ['title', 'text', 'pub_date', 'image', 'video']
+
+    def to_representation(self, instance):
+        data = super().to_representation(instance)
+        lang = self.context.get('lang')
+        if lang == 'ru':
+            data['text'] = instance.text_ru
+        elif lang == 'pl':
+            data['text'] = instance.text_pl
+        else:
+            data['text'] = instance.text_en
+        return data
