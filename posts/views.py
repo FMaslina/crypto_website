ba67b414fd4d68e39465efe50ea9f2b70f8435b4
FMaslina <FMaslina@yandex.ru>
2024-03-04 20:01:25 +0700

    Update PostGetView

--- posts/views.py
+++ posts/views.py
@@ -1,11 +1,21 @@
-from rest_framework import generics
+from rest_framework import generics, status
+from rest_framework.response import Response
+
 from posts.models import Post
 from posts.serializers import PostSerializer
+from posts.utils import get_translate
 
 
 class PostGetView(generics.RetrieveAPIView):
     serializer_class = PostSerializer
     queryset = Post.objects.all()
+
+    def get(self, request, *args, **kwargs):
+        post = self.queryset.get(pk=kwargs['pk'])
+        lang = request.GET['lang']
+        serialized_post = self.serializer_class(post, context={'lang': lang}).data
+
+        return Response(status=status.HTTP_200_OK, data=serialized_post)
 
 
 class PostListView(generics.ListAPIView):
