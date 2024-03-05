76075bb6d9c4e7eaab934c071667321aad46212f
FMaslina <FMaslina@yandex.ru>
2024-03-05 16:43:46 +0700

    Update urls

--- posts/urls.py
+++ posts/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from posts.views import PostListView, PostGetView, PostListTestView
 
 urlpatterns = [
-    path('news/list', PostListTestView.as_view(), name='news_list'),
+    path('news/list', PostListView.as_view(), name='news_list'),
     path('news/get/<int:pk>', PostGetView.as_view(), name='news_get')
 ]
